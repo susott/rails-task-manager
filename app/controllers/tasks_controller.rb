609cf6d92951d41fdb502644d494b4d963b1b982
susott <37674058+susott@users.noreply.github.com>
2018-05-08 15:04:30 +0200

    DRYed the routes and the controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_id, only: [:edit, :show, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,18 +19,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
     # raise
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -40,6 +38,10 @@ class TasksController < ApplicationController
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_id
+        @task = Task.find(params[:id])
   end
 
 end
